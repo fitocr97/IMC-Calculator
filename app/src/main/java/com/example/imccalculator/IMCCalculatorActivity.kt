4925eb1fbb642c61e388e8f005cefa3e3b4f9f79
Rodolfo <rodolfomora97@gmail.com>
2023-05-15 14:25:27 -0600

    change the weight in the interface by pressing the plus or minus button

--- app/src/main/java/com/example/imccalculator/IMCCalculatorActivity.kt
+++ app/src/main/java/com/example/imccalculator/IMCCalculatorActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
@@ -13,6 +14,9 @@ class IMCCalculatorActivity : AppCompatActivity() {
     private var isMaleSelected: Boolean = true
     private var isFemaleSelected: Boolean = false
 
+    //initialized variables
+    private var currentWeight:Int = 65
+
     //declare components to initialize them and use them anywhere
     //gender
     private lateinit var viewMale:CardView
@@ -20,6 +24,10 @@ class IMCCalculatorActivity : AppCompatActivity() {
     //height
     private lateinit var tvHeight:TextView
     private lateinit var rsHeight:RangeSlider
+    //weight
+    private lateinit var tvWeight:TextView
+    private lateinit var btnMinusWeight:FloatingActionButton
+    private lateinit var btnPlusWeight:FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +41,13 @@ class IMCCalculatorActivity : AppCompatActivity() {
     private fun initComponents() {
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
+
         tvHeight = findViewById(R.id.tvHeight)
         rsHeight = findViewById(R.id.rsHeight)
+
+        tvWeight = findViewById(R.id.tvWeight)
+        btnMinusWeight = findViewById(R.id.btnMinusWeight)
+        btnPlusWeight = findViewById(R.id.btnPlusWeight)
     }
 
     //function to know when a card is pressed
@@ -49,12 +62,27 @@ class IMCCalculatorActivity : AppCompatActivity() {
             setGenderColor()
         }
 
-        //know when the slider moved
+        //know when the slider moved, To change the height value
         rsHeight.addOnChangeListener { _, value, _ ->
             val df = DecimalFormat("#.##")
             val result = df.format(value)
             tvHeight.text = result.toString()
         }
+
+        //listeners to modify the value of the weight, each time the button is pressed the listener acts
+        btnPlusWeight.setOnClickListener{
+            currentWeight += 1
+            setWeight()
+        }
+        btnMinusWeight.setOnClickListener {
+            currentWeight -= 1
+            setWeight()
+        }
+    }
+
+    //shows the current weight the graphic part
+    private fun setWeight() {
+        tvWeight.text = currentWeight.toString()
     }
 
     //change the status of the gender, if the man is selected and the woman is selected, it is changed to false and vice versa
@@ -83,6 +111,7 @@ class IMCCalculatorActivity : AppCompatActivity() {
     //start the interface
     private fun initUI(){
         setGenderColor()
+        setWeight() //
     }
 
 
